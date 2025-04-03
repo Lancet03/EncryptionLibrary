e218b4d1e093ef56ebefeec5f91166b47b680756
Lancet03 <ilja.efremov-2003@mail.ru>
2025-04-04 00:26:31 +0300

    Add project files.

---
+++ EncryptionLibrary/Class1.cs
@@ -0,0 +1,170 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EncryptionLibrary
+{
+    public static class Hashing
+    {
+        public static string Hash(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+    }
+
+    public class RSA
+    {
+        private PrivateKey privateKey;
+        private PublicKey publicKey;
+
+        public RSA(int salt = 100)
+        {
+            GenerateKeys(salt);
+        }
+
+        public RSA(PublicKey publicKey, PrivateKey privateKey)
+        {
+            this.publicKey = publicKey;
+            this.privateKey = privateKey;
+        }
+
+        public class PublicKey
+        {
+            public BigInteger e;
+            public BigInteger n;
+            public PublicKey(BigInteger e, BigInteger n)
+            {
+                this.e = e;
+                this.n = n;
+            }
+        }
+
+        public class PrivateKey
+        {
+            public BigInteger d;
+            public BigInteger n;
+            public PrivateKey(BigInteger d, BigInteger n)
+            {
+                this.d = d;
+                this.n = n;
+            }
+        }
+
+        public PrivateKey GetPrivateKey() => privateKey;
+        public void SetPrivateKey(PrivateKey privKey) => privateKey = privKey;
+
+
+        public PublicKey GetPublicKey() => publicKey;
+        public void SetPublicKey(PublicKey pubKey) => publicKey = pubKey;
+
+        static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            if (n <= 3) return true;
+            if (n % 2 == 0 || n % 3 == 0) return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static int NextPrime(int start)
+        {
+            int candidate = start + 1;
+            while (true)
+            {
+                if (IsPrime(candidate))
+                    return candidate;
+                candidate++;
+            }
+        }
+
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+            return a;
+        }
+
+        public static BigInteger ModInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m, t, q;
+            BigInteger x0 = 0, x1 = 1;
+
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                q = a / m;
+                t = m;
+
+                m = a % m;
+                a = t;
+                t = x0;
+
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            return x1 < 0 ? x1 + m0 : x1;
+        }
+
+        public void GenerateKeys(int salt = 100)
+        {
+            // Генерация ключей
+            int p = NextPrime(salt);
+            int q = NextPrime(p);
+            BigInteger n = p * q;
+            BigInteger phi = (p - 1) * (q - 1);
+            BigInteger e = 65537;
+            Console.WriteLine($"phi = {phi}");
+
+            if (GCD((int)e, (int)phi) != 1)
+                e = 3;
+
+            BigInteger d = ModInverse(e, phi);
+
+            Console.WriteLine($"Открытый ключ: (e = {e}, n = {n})");
+            Console.WriteLine($"Закрытый ключ: (d = {d}, n = {n})");
+
+            privateKey = new PrivateKey(d, n);
+            publicKey = new PublicKey(e, n);
+        }
+
+        public List<BigInteger> EncryptString(string message)
+        {
+            var encrypted = new List<BigInteger>();
+            foreach (char ch in message)
+            {
+                BigInteger m = ch;
+                encrypted.Add(BigInteger.ModPow(m, publicKey.e, publicKey.n));
+            }
+            return encrypted;
+        }
+
+        public string DecryptString(List<BigInteger> encrypted)
+        {
+            var result = new StringBuilder();
+            foreach (var c in encrypted)
+            {
+                BigInteger m = BigInteger.ModPow(c, privateKey.d, privateKey.n);
+                result.Append((char)(int)m);
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ LibraryTestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EncryptionLibrary;
+
+EncryptionLibrary.RSA rsa = new EncryptionLibrary.RSA();
+rsa.GenerateKeys();
+
+Console.Write("Введите строку для шифрования: ");
+string input = Console.ReadLine();
+
+var encrypted = rsa.EncryptString(input);
+Console.WriteLine("\nЗашифрованная строка (в виде чисел):");
+Console.WriteLine(string.Join(" ", encrypted));
+
+string decrypted = rsa.DecryptString(encrypted);
+Console.WriteLine("\nРасшифрованная строка:");
+Console.WriteLine(decrypted);
